62480bebbf06fcbbc9c59bc719e55cf5d13e8a4e
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2024-04-18 17:49:30 +0200

    Setup search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
+type Cuisine = {
+  name: string;
+  uniqueName: string;
+}
+type Restaurant = {
+  name: string;
+  cuisines: Cuisine[];
+  rating: number;
+  address: {
+    firstLine: string;
+    postalCode: string;
+    city: string;
+  }
+}
 function App() {
+  const [postCode, setPostCode] = useState("");
+  const [restaurants, setRestaurants] = useState<Restaurant[]>([])
+
+  const handlePostCode = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPostCode(e.target.value)
+  }
+
+  const handleSearch = () => {
+    if (postCode) {
+      axios.get(`http://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postCode}`)
+        .then((data: any) => {
+          const allRestaurants = data.restaurants.slice(0, 10).map((item: any) => {
+            const restaurant: Restaurant = {
+              name: item.name,
+              cuisines: item.cuisines,
+              rating: item.rating.starRating,
+              address: {
+                firstLine: item.address.firstLine,
+                postalCode: item.address.postalCode,
+                city: item.adddress.city
+              }
+            }
+            return restaurant;
+          })
+
+          setRestaurants(allRestaurants)
+          return allRestaurants
+        })
+        .then((result) => console.log("success!"))
+        .catch((err) => console.log("Fetch error:", err))
+    }
+  }
+
+  useEffect(() => {
+    console.log(postCode)
+  }, [postCode])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Order takeaway in your area</h1>
+      <p>Find restaurants near you</p>
+
+      <label htmlFor="">
+        <input type="text" name="" id="" onChange={(e) => handlePostCode(e)} />
+        <button onClick={handleSearch}>Search</button>
+      </label>
     </div>
   );
 }
